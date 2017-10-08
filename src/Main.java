289b2c217d29fa04d332da351a297f83d61d5dd3
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2017-10-08 16:53:09 +0000

    Identifica estados inuteis

--- src/Main.java
+++ src/Main.java
@@ -3,119 +3,174 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Stack;
 
 public class Main {
 
-	private static String[][] matrizAdj;	// representação do AFD por matriz de adjacência
-	private static boolean[] saidas;		// indica quais estados são de aceitação
-	private static int sigma;				// tamanho do alfabeto
-	private static int ini;					// estado inicial
+    private static String[][] matrizAdj;    // representação do AFD por matriz de adjacência
+    private static boolean[] saidas;        // indica quais estados são de aceitação
+    private static int sigma;                // tamanho do alfabeto
+    private static int ini;                    // estado inicial
 
-	private static void LeEntrada(String dir){
+    private static void LeEntrada(String dir) {
 
-		//leitor de arquivos
-		File file = new File(dir);
-		BufferedReader reader = null;
+        //leitor de arquivos
+        File file = new File(dir);
+        BufferedReader reader = null;
 
-		try {
+        try {
 
-			// lê a primeira linha da entrada
-			reader = new BufferedReader(new FileReader(file));
-			String linha = reader.readLine();
+            // lê a primeira linha da entrada
+            reader = new BufferedReader(new FileReader(file));
+            String linha = reader.readLine();
 
-			// quebra a linha lida em parâmetros separando pelo caractere espaço
-			String[] param = linha.split(" ");
+            // quebra a linha lida em parâmetros separando pelo caractere espaço
+            String[] param = linha.split(" ");
 
-			// inicializa a matriz de adjacência e de aceitação com tamanho n
-			int n = Integer.parseInt(param[0]);
-			matrizAdj = new String[n][n];
-			saidas = new boolean[n];
+            // inicializa a matriz de adjacência e de aceitação com tamanho n
+            int n = Integer.parseInt(param[0]);
+            matrizAdj = new String[n][n];
+            saidas = new boolean[n];
 
-			// armazena o sigma (tamanho do alfabeto)
-			sigma = Integer.parseInt(param[1]);
+            // armazena o sigma (tamanho do alfabeto)
+            sigma = Integer.parseInt(param[1]);
 
-			// grava estado de entrada
-			ini = Integer.parseInt(param[2]);
+            // grava estado de entrada
+            ini = Integer.parseInt(param[2]);
 
-			// lê segunda linha
-			linha = reader.readLine();
-			param = linha.split(" ");
+            // lê segunda linha
+            linha = reader.readLine();
+            param = linha.split(" ");
 
-			// grava estados de aceitação
-			for (int i = 0; i < n; i++){
-				saidas[i] = (param[i].equals("1"));
-			}
+            // grava estados de aceitação
+            for (int i = 0; i < n; i++) {
+                saidas[i] = (param[i].equals("1"));
+            }
 
-			// lê as demais linhas que contém informações sobre as transições
-			for (int i = 0; i < n; i++){
+            // lê as demais linhas que contém informações sobre as transições
+            for (int i = 0; i < n; i++) {
 
-				linha = reader.readLine();
-				param = linha.split(" ");
+                linha = reader.readLine();
+                param = linha.split(" ");
 
-				for (int j = 0; j < sigma; j++){
+                for (int j = 0; j < sigma; j++) {
 
-					//se o valor for -1, não há transição a ser adicionada
-					if (!param[j].equals("-1")) {
+                    //se o valor for -1, não há transição a ser adicionada
+                    if (!param[j].equals("-1")) {
 
-						//qualquer outro valor significa que há uma transição partindo do estado i e chegando no estado param[j] ao receber o valor j
-						int y = Integer.parseInt(param[j]);
-						if (matrizAdj[i][y] == null) matrizAdj[i][y] = "";
-						matrizAdj[i][y] += (matrizAdj[i][y].isEmpty() ? j : ";" + j);
-					}
-				}
-			}
+                        //qualquer outro valor significa que há uma transição partindo do estado i e chegando no estado param[j] ao receber o valor j
+                        int y = Integer.parseInt(param[j]);
+                        if (matrizAdj[i][y] == null) matrizAdj[i][y] = "";
+                        matrizAdj[i][y] += (matrizAdj[i][y].isEmpty() ? j : ";" + j);
+                    }
+                }
+            }
 
-			//tratamento das exceções
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+            //tratamento das exceções
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
 
-		} catch (IOException e) {
-			e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
 
-		} finally {
-			try {
+        } finally {
+            try {
 
-				if (reader != null) reader.close();
+                if (reader != null) reader.close();
 
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private static String TipoEstado(int i){
+    private static String TipoEstado(int i) {
 
-		String tipoEstado;
+        String tipoEstado;
 
-		//testa se é estado inicial
-		if (i == ini) tipoEstado = "I";
-		//se não for, testa se é estado de aceitação
-		else if (saidas[i]) tipoEstado = "A";
-		//se não, só pode ser um estado neutro
-		else tipoEstado = "N";
+        //testa se é estado inicial
+        if (i == ini) tipoEstado = "I";
+            //se não for, testa se é estado de aceitação
+        else if (saidas[i]) tipoEstado = "A";
+            //se não, só pode ser um estado neutro
+        else tipoEstado = "N";
 
-		return tipoEstado;
-	}
+        return tipoEstado;
+    }
 
-	private static void TestaConstrucao(){
+    // Implementa a busca em profundidade (DFS) para determinar os estados inuteis
+    private static void EstadosInuteis(ArrayList<Integer> estadosInuteis) {
+        // Assume inicialmente que todos os estados sao inuteis
+        for (int i = 0; i < saidas.length; i++) {
+            estadosInuteis.add(i);
+        }
 
-		//saída para testar a construção da matriz de adjacência
-		String linha = "";
-		for (int i = 0; i < matrizAdj.length; i++){
-			for (int j = 0; j < matrizAdj.length; j++){
-				//se não houver nada em matriz[i,j], não há uma transição saindo do estado i e indo para o estado j
-				if (matrizAdj[i][j] == null) matrizAdj[i][j] = "-1";
-				//(origemTransição [tipoEstado] -> destinoTransição [tipoEstado]) {valorTransição1;valorTransição2,...,valorTransiçãoN}
-				linha += "(" + i + " [" + TipoEstado(i) + "] -> " + j + " [" + TipoEstado(j) + "]) {" + matrizAdj[i][j] + "} ";
-			}
-			System.out.println(linha);
-			linha = "";
-		}
-	}
+        // Busca em profundidade para preencher um vetor de estados que conseguiram ser visitados
+        Stack<Integer> pilha = new Stack<>();
+        ArrayList<Integer> visitado = new ArrayList<>();
 
-	public static void main(String[] args) {
+        pilha.push(ini);
 
-		LeEntrada(args[0]);
-		TestaConstrucao();
-	}
+        // Percorre a matriz enquanto a pilha não estiver vazia
+        while (!pilha.isEmpty()) {
+            int estado = pilha.pop();
+            visitado.add(estado);
+
+            for (int i = 0; i < matrizAdj.length; i++) {
+                // Se o estado nao estiver na pilha ou nos visitados, adicione na pilha
+                if (matrizAdj[estado][i] != null) {
+                    if (!pilha.contains(i) && !visitado.contains(i)) {
+                        pilha.push(i);
+                    }
+                }
+            }
+        }
+
+        // Retira os estados que conseguiram ser visitados com a busca em profundidade
+        for (Integer estado : visitado) {
+            estadosInuteis.remove(estado);
+        }
+    }
+
+    private static void TestaEstadosInuteis(ArrayList<Integer> estadosInuteis) {
+        if (estadosInuteis.size() == 0) {
+            System.out.println("Nao existe estados inuteis");
+            return;
+        }
+
+        // Mostra os indices dos estados inuteis
+        System.out.print("Estados inuteis: ");
+        for (Integer estado : estadosInuteis) {
+            System.out.print(estado + " ");
+        }
+        System.out.println();
+    }
+    private static void TestaConstrucao() {
+
+        //saída para testar a construção da matriz de adjacência
+        String linha = "";
+        for (int i = 0; i < matrizAdj.length; i++) {
+            for (int j = 0; j < matrizAdj.length; j++) {
+                //se não houver nada em matriz[i,j], não há uma transição saindo do estado i e indo para o estado j
+                if (matrizAdj[i][j] == null) matrizAdj[i][j] = "-1";
+                //(origemTransição [tipoEstado] -> destinoTransição [tipoEstado]) {valorTransição1;valorTransição2,...,valorTransiçãoN}
+                linha += "(" + i + " [" + TipoEstado(i) + "] -> " + j + " [" + TipoEstado(j) + "]) {" + matrizAdj[i][j] + "} ";
+            }
+            System.out.println(linha);
+            linha = "";
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> estadosInuteis = new ArrayList<>();
+
+        LeEntrada(args[0]);
+        EstadosInuteis(estadosInuteis);
+
+        TestaEstadosInuteis(estadosInuteis);
+        TestaConstrucao();
+    }
 }
